X-oss-byte/Open-workerd-source

JS RPC event/capability/demo

src/workerd/api/actor.c++
@@ -11,6 +11,7 @@
 #include <capnp/compat/http-over-capnp.h>
 #include <capnp/schema.h>
 #include <capnp/message.h>
+#include <workerd/api/js-rpc-service.h>
 
 namespace workerd::api {
 
@@ -97,7 +98,7 @@ private:
   kj::Maybe<kj::Own<IoChannelFactory::ActorChannel>> actorChannel;
 };
 
-jsg::Ref<Fetcher> ColoLocalActorNamespace::get(kj::String actorId) {
+jsg::Ref<DurableObjectRpcStub> ColoLocalActorNamespace::get(kj::String actorId) {
   JSG_REQUIRE(actorId.size() > 0 && actorId.size() <= 2048, TypeError,
       "Actor ID length must be in the range [1, 2048].");
 
@@ -107,12 +108,50 @@ jsg::Ref<Fetcher> ColoLocalActorNamespace::get(kj::String actorId) {
       channel, kj::mv(actorId));
   auto outgoingFactory = context.addObject(kj::mv(factory));
 
-  bool isInHouse = true;
-  return jsg::alloc<Fetcher>(
-      kj::mv(outgoingFactory), Fetcher::RequiresHostAndProtocol::YES, isInHouse);
+  return jsg::alloc<DurableObjectRpcStub>(
+      kj::mv(outgoingFactory), Fetcher::RequiresHostAndProtocol::YES, IsInHouse::YES);
 }
 
 // =======================================================================================
+jsg::Promise<jsg::V8Ref<v8::Value>> DurableObjectRpcStub::getRpcAndCallRemote(
+    jsg::Lock& js,
+    kj::String methodName,
+    jsg::Optional<kj::Array<jsg::Value>> args) {
+  auto& ioContext = IoContext::current();
+  // TODO(now): Just a reminder for the future, rename this operation name.
+  auto worker = getClient(ioContext, nullptr, "rpcTest"_kjc);
+  auto event = kj::heap<api::GetJsRpcTargetCustomEventImpl>(9);
+
+  rpc::JsRpcTarget::Client client = kj::mv(event->clientCap);
+  auto builder = client.callRequest();
+  builder.setMethodName(kj::mv(methodName));
+  // TODO(now): We should serialize the arguments we want to pass, and set them as well.
+  builder.initSerializedArgs().initV8Serialized(0);
+  auto callResult = builder.send();
+  auto customEventResult = worker->customEvent(kj::mv(event)).attach(kj::mv(worker), kj::mv(client));
+
+  return ioContext.awaitIo(js, kj::mv(callResult), [&ioContext, custom = kj::mv(customEventResult)]
+      (jsg::Lock& js, capnp::Response<rpc::JsRpcTarget::CallResults> rpcResult) mutable
+          -> jsg::Promise<jsg::V8Ref<v8::Value>> {
+
+    // TODO(now): Can we skip awaiting and checking the custom event result? Maybe just attach the
+    // promise to callResult above?
+    return ioContext.awaitIo(js, kj::mv(custom), [rpcResult=kj::mv(rpcResult)]
+        (jsg::Lock& js, WorkerInterface::CustomEvent::Result customResult) {
+      auto serializedResult = rpcResult.getResult().getV8Serialized();
+      auto deserialized = jsg::deserializeV8Rpc(js, kj::heapArray(serializedResult.asBytes()));
+      if (customResult.outcome == EventOutcome::OK) {
+        // TODO(now): Need to think about how we handle returning vs. throwing error from remote.
+        if (deserialized.isNativeError()) {
+          JSG_FAIL_REQUIRE(Error, deserialized.toString(js));
+        } else {
+          return jsg::Value(js.v8Isolate, deserialized);
+        }
+      }
+      KJ_UNREACHABLE;
+    });
+  });
+}
 
 kj::String DurableObjectId::toString() {
   return id->toString();

src/workerd/api/actor.h
@@ -20,13 +20,45 @@ namespace workerd {
 
 namespace workerd::api {
 
+enum IsInHouse {
+  YES,
+  NO,
+};
+
+// TODO(soon): This will eventually become the Proxy object.
+// This class extends Fetcher with the JS RPC call function, but doesn't go as far as DurableObject.
+// We don't want to define RPC stuff on Fetcher itself, and since ColoLocalActorNamespace::get()
+// doesn't return a DurableObject, we can't define it there either. Instead, we use a class that's
+// somewhere in-between.
+class DurableObjectRpcStub : public Fetcher {
+public:
+  DurableObjectRpcStub(
+      IoOwn<OutgoingFactory> outgoingFactory,
+      RequiresHostAndProtocol requiresHost,
+      IsInHouse inHouse)
+    : Fetcher(kj::mv(outgoingFactory), requiresHost, (inHouse == YES) /* isInHouse */) {}
+
+  jsg::Promise<jsg::V8Ref<v8::Value>> getRpcAndCallRemote(
+      jsg::Lock& js, kj::String methodName, jsg::Optional<kj::Array<jsg::Value>> args=nullptr);
+  // TODO(now): We won't merge with this test function -- it only exists for testing remote calls.
+  // Get an rpc service and call the remote method.
+  // This will evolve once we have a proxy type and can intercept method calls.
+
+  JSG_RESOURCE_TYPE(DurableObjectRpcStub, CompatibilityFlags::Reader flags) {
+    JSG_INHERIT(Fetcher);
+    if (flags.getJsRpcStub()) {
+      JSG_METHOD(getRpcAndCallRemote);
+    }
+  }
+};
+
 // A capability to an ephemeral Actor namespace.
 class ColoLocalActorNamespace: public jsg::Object {
 public:
   ColoLocalActorNamespace(uint channel)
     : channel(channel) {}
 
-  jsg::Ref<Fetcher> get(kj::String actorId);
+  jsg::Ref<DurableObjectRpcStub> get(kj::String actorId);
 
   JSG_RESOURCE_TYPE(ColoLocalActorNamespace) {
     JSG_METHOD(get);
@@ -69,19 +101,19 @@ private:
 };
 
 // Stub object used to send messages to a remote durable object.
-class DurableObject: public Fetcher {
+class DurableObject: public DurableObjectRpcStub {
 
 public:
   DurableObject(jsg::Ref<DurableObjectId> id, IoOwn<OutgoingFactory> outgoingFactory,
                 RequiresHostAndProtocol requiresHost)
-    : Fetcher(kj::mv(outgoingFactory), requiresHost, true /* isInHouse */),
+    : DurableObjectRpcStub(kj::mv(outgoingFactory), requiresHost, IsInHouse::YES),
       id(kj::mv(id)) {}
 
   jsg::Ref<DurableObjectId> getId() { return id.addRef(); };
   jsg::Optional<kj::StringPtr> getName() { return id->getName(); }
 
   JSG_RESOURCE_TYPE(DurableObject) {
-    JSG_INHERIT(Fetcher);
+    JSG_INHERIT(DurableObjectRpcStub);
 
     JSG_READONLY_INSTANCE_PROPERTY(id, getId);
     JSG_READONLY_INSTANCE_PROPERTY(name, getName);
@@ -196,6 +228,7 @@ private:
 };
 
 #define EW_ACTOR_ISOLATE_TYPES                      \
+  api::DurableObjectRpcStub,                        \
   api::ColoLocalActorNamespace,                     \
   api::DurableObject,                               \
   api::DurableObjectId,                             \

gogod3v-test-org/workerd

src/workerd/api/actor.c++
@@ -11,6 +11,7 @@
 #include <capnp/compat/http-over-capnp.h>
 #include <capnp/schema.h>
 #include <capnp/message.h>
+#include <workerd/api/js-rpc-service.h>
 
 namespace workerd::api {
 
@@ -97,7 +98,7 @@ private:
   kj::Maybe<kj::Own<IoChannelFactory::ActorChannel>> actorChannel;
 };
 
-jsg::Ref<Fetcher> ColoLocalActorNamespace::get(kj::String actorId) {
+jsg::Ref<DurableObjectRpcStub> ColoLocalActorNamespace::get(kj::String actorId) {
   JSG_REQUIRE(actorId.size() > 0 && actorId.size() <= 2048, TypeError,
       "Actor ID length must be in the range [1, 2048].");
 
@@ -107,12 +108,56 @@ jsg::Ref<Fetcher> ColoLocalActorNamespace::get(kj::String actorId) {
       channel, kj::mv(actorId));
   auto outgoingFactory = context.addObject(kj::mv(factory));
 
-  bool isInHouse = true;
-  return jsg::alloc<Fetcher>(
-      kj::mv(outgoingFactory), Fetcher::RequiresHostAndProtocol::YES, isInHouse);
+  return jsg::alloc<DurableObjectRpcStub>(
+      kj::mv(outgoingFactory), Fetcher::RequiresHostAndProtocol::YES, IsInHouse::YES);
 }
 
 // =======================================================================================
+jsg::Promise<jsg::JsRef<jsg::JsValue>> DurableObjectRpcStub::getRpcAndCallRemote(
+    jsg::Lock& js,
+    kj::String methodName,
+    jsg::Optional<jsg::Arguments<jsg::JsValue>> args) {
+  auto& ioContext = IoContext::current();
+  // TODO(now): Just a reminder for the future, rename this operation name.
+  auto worker = getClient(ioContext, nullptr, "rpcTest"_kjc);
+  auto event = kj::heap<api::GetJsRpcTargetCustomEventImpl>(9);
+
+  rpc::JsRpcTarget::Client client = event->getCap();
+  auto builder = client.callRequest();
+  builder.setMethodName(kj::mv(methodName));
+
+  // If we have arguments, serialize them.
+  KJ_IF_SOME(arguments, args) {
+    if (arguments.size() > 0) {
+      auto ser = jsg::serializeV8Rpc(js, js.arr(arguments.asPtr()));
+      builder.initSerializedArgs().setV8Serialized(kj::mv(ser));
+    }
+  }
+  auto callResult = builder.send();
+  auto customEventResult = worker->customEvent(kj::mv(event)).attach(kj::mv(worker), kj::mv(client));
+
+  return ioContext.awaitIo(js, kj::mv(callResult), [&ioContext, custom = kj::mv(customEventResult)]
+      (jsg::Lock& js, capnp::Response<rpc::JsRpcTarget::CallResults> rpcResult) mutable
+          -> jsg::Promise<jsg::JsRef<jsg::JsValue>> {
+
+    // TODO(now): Can we skip awaiting and checking the custom event result? Maybe just attach the
+    // promise to callResult above?
+    return ioContext.awaitIo(js, kj::mv(custom), [rpcResult=kj::mv(rpcResult)]
+        (jsg::Lock& js, WorkerInterface::CustomEvent::Result customResult) {
+      auto serializedResult = rpcResult.getResult().getV8Serialized();
+      auto deserialized = jsg::deserializeV8Rpc(js, kj::heapArray(serializedResult.asBytes()));
+      if (customResult.outcome == EventOutcome::OK) {
+        // TODO(now): Need to think about how we handle returning vs. throwing error from remote.
+        if (deserialized.isNativeError()) {
+          JSG_FAIL_REQUIRE(Error, deserialized.toString(js));
+        } else {
+          return deserialized.addRef(js);
+        }
+      }
+      KJ_UNREACHABLE;
+    });
+  });
+}
 
 kj::String DurableObjectId::toString() {
   return id->toString();

src/workerd/api/actor.h
@@ -20,13 +20,45 @@ namespace workerd {
 
 namespace workerd::api {
 
+enum IsInHouse {
+  YES,
+  NO,
+};
+
+// TODO(soon): This will eventually become the Proxy object.
+// This class extends Fetcher with the JS RPC call function, but doesn't go as far as DurableObject.
+// We don't want to define RPC stuff on Fetcher itself, and since ColoLocalActorNamespace::get()
+// doesn't return a DurableObject, we can't define it there either. Instead, we use a class that's
+// somewhere in-between.
+class DurableObjectRpcStub : public Fetcher {
+public:
+  DurableObjectRpcStub(
+      IoOwn<OutgoingFactory> outgoingFactory,
+      RequiresHostAndProtocol requiresHost,
+      IsInHouse inHouse)
+    : Fetcher(kj::mv(outgoingFactory), requiresHost, (inHouse == YES)) {}
+
+  jsg::Promise<jsg::JsRef<jsg::JsValue>> getRpcAndCallRemote(
+      jsg::Lock& js, kj::String methodName, jsg::Optional<jsg::Arguments<jsg::JsValue>> args=nullptr);
+  // TODO(now): We won't merge with this test function -- it only exists for testing remote calls.
+  // Get an rpc service and call the remote method.
+  // This will evolve once we have a proxy type and can intercept method calls.
+
+  JSG_RESOURCE_TYPE(DurableObjectRpcStub, CompatibilityFlags::Reader flags) {
+    JSG_INHERIT(Fetcher);
+    if (flags.getJsRpcStub()) {
+      JSG_METHOD(getRpcAndCallRemote);
+    }
+  }
+};
+
 // A capability to an ephemeral Actor namespace.
 class ColoLocalActorNamespace: public jsg::Object {
 public:
   ColoLocalActorNamespace(uint channel)
     : channel(channel) {}
 
-  jsg::Ref<Fetcher> get(kj::String actorId);
+  jsg::Ref<DurableObjectRpcStub> get(kj::String actorId);
 
   JSG_RESOURCE_TYPE(ColoLocalActorNamespace) {
     JSG_METHOD(get);
@@ -69,19 +101,19 @@ private:
 };
 
 // Stub object used to send messages to a remote durable object.
-class DurableObject: public Fetcher {
+class DurableObject: public DurableObjectRpcStub {
 
 public:
   DurableObject(jsg::Ref<DurableObjectId> id, IoOwn<OutgoingFactory> outgoingFactory,
                 RequiresHostAndProtocol requiresHost)
-    : Fetcher(kj::mv(outgoingFactory), requiresHost, true /* isInHouse */),
+    : DurableObjectRpcStub(kj::mv(outgoingFactory), requiresHost, IsInHouse::YES),
       id(kj::mv(id)) {}
 
   jsg::Ref<DurableObjectId> getId() { return id.addRef(); };
   jsg::Optional<kj::StringPtr> getName() { return id->getName(); }
 
   JSG_RESOURCE_TYPE(DurableObject) {
-    JSG_INHERIT(Fetcher);
+    JSG_INHERIT(DurableObjectRpcStub);
 
     JSG_READONLY_INSTANCE_PROPERTY(id, getId);
     JSG_READONLY_INSTANCE_PROPERTY(name, getName);
@@ -196,6 +228,7 @@ private:
 };
 
 #define EW_ACTOR_ISOLATE_TYPES                      \
+  api::DurableObjectRpcStub,                        \
   api::ColoLocalActorNamespace,                     \
   api::DurableObject,                               \
   api::DurableObjectId,                             \